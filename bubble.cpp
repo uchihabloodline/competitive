69ff2af447b2a0b07663c81e5a0550cc01ef3193
shivam <geetapandey595@gmail.com>
2018-01-19 19:15:06 +0530

    sorting-1

---
+++ bubble.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	int n, a[20],temp;
+	cin>>n;
+
+			for (int i=0;i<n;i++){
+				cin>>a[i];
+			}
+
+			  for(int i=1;i<n;i++){
+
+				for(int j=0;j<n-i;j++){
+
+					if(a[j] > a[j+1]){
+
+					    temp = a[j];
+					       a[j] = a[j+1];
+					    a[j+1] = temp;
+
+					}
+				}
+			}
+
+		for(int i=0;i < n ;i++){
+		  cout<<a[i]<<" ";
+
+					}
+			return 0;
+}
